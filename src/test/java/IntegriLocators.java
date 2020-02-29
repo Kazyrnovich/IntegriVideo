5ede4ff84fe61a28ed22eef249475c4e12a0975e
Kaz <choco74@mail.ru>
2020-02-29 17:20:49 +0300

    Initial commit

---
+++ src/test/java/IntegriLocators.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class IntegriLocators {
+    @Test
+    public void categoryLocators() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://dev.integrivideo.com/demo/5e5a6a8baa6a522b44063e6b");
+        browser.manage().window().maximize();
+
+    }
+
+}
