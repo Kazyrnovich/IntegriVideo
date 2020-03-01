678da2bb04dcd8c4176036ee0a9cfddd1314127e
Kaz <choco74@mail.ru>
2020-03-01 15:09:14 +0300

    Двадцать тестов

--- src/test/java/IntegriLocators.java
+++ src/test/java/IntegriLocators.java
@@ -12,10 +12,69 @@ public class IntegriLocators {
         browser.get("https://dev.integrivideo.com/demo/5e5a6a8baa6a522b44063e6b");
         browser.manage().window().maximize();
 
-        WebElement element = browser.findElement(By.cssSelector(".navbar-brand.logo"));
-        element.click();
-        
+        WebElement element1 = browser.findElement(By.cssSelector(".navbar-brand.logo"));
+        element1.click();
+
+        WebElement element2 = browser.findElement(By.cssSelector(".btn"));
+        element2.click();
+
+        WebElement element3 = browser.findElement(By.xpath("//*[@id='login-form']/div/input[1]"));
+        element3.click();
+
+        WebElement element4 = browser.findElement(By.xpath("//*[@id=\"login-form\"]/div/input[2]"));
+        element4.click();
+
+        WebElement element5 = browser.findElement(By.linkText("Sign up"));
+        element5.click();
+
+        WebElement element6 = browser.findElement(By.linkText("Log in"));
+        element6.click();
+
+        WebElement element7 = browser.findElement(By.cssSelector(".btn.btn-primary"));
+        element7.click();
+
+        WebElement element8 = browser.findElement(By.cssSelector(".form-control"));
+        element8.click();
+
+        browser.get("https://dev.integrivideo.com/demo/5e5a6a8baa6a522b44063e6b");
+        browser.manage().window().maximize();
+        WebElement element9 = browser.findElement(By.xpath("//*[text()='Invite users to chat']"));
+        element9.click();
+
+        WebElement element10 = browser.findElement(By.cssSelector("[target=_blank]"));
+        element10.click();
+
+        WebElement element11 = browser.findElement(By.cssSelector(".component-code"));
+        element11.click();
+
+        WebElement element12 = browser.findElement(By.cssSelector(".iv-icon.iv-icon-paper-plane"));
+        element12.click();
+
+        WebElement element13 = browser.findElement(By.cssSelector(".iv-icon.iv-icon-camera"));
+        element13.click();
+
+        WebElement element14 = browser.findElement(By.cssSelector(".integri-video.public"));
+        element14.click();
+
+        WebElement element15 = browser.findElement(By.cssSelector(".iv-icon.iv-icon-cog"));
+        element15.click();
+
+        WebElement element16 = browser.findElement(By.cssSelector(".integri-chat-bottom-text"));
+        element16.click();
+
+        WebElement element17 = browser.findElement(By.linkText("For developers"));
+        element17.click();
+
+        WebElement element18 = browser.findElement(By.linkText("Terms of use"));
+        element18.click();
+
+        WebElement element19 = browser.findElement(By.linkText("Privacy policy"));
+        element19.click();
+
+        WebElement element20 = browser.findElement(By.linkText("Blog"));
+        element20.click();
 
         browser.quit();
+
     }
 }
